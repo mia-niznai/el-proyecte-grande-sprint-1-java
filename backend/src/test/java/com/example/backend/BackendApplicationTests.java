cf7ad58988748b7cfd707e7b7719b6dab79bfcc1
mia-niznai <maria.popa.8735@gmail.com>
2023-10-03 14:22:44 +0300

    redo backend project

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.codecool.epg;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
