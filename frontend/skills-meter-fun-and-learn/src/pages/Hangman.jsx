ff2d0ebc9def52473f63cca5744121250ef0fde7
Teodor <teodormihai2000@gmail.com>
2023-10-12 15:04:09 +0300

    hangman page

--- frontend/skills-meter-fun-and-learn/src/pages/Hangman.jsx
+++ frontend/skills-meter-fun-and-learn/src/pages/Hangman.jsx
@@ -1,6 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import Header from '../components/hangmanComp/comps/Header';
+import Figure from '../components/hangmanComp/comps/Figure';
+import WrongLetters from '../components/hangmanComp/comps/WrongLetters';
+import Word from '../components/hangmanComp/comps/Word';
+import Popup from '../components/hangmanComp/comps/Popup';
+import Notification from '../components/hangmanComp/comps/Notification';
+import { showNotification as show } from '../components/hangmanComp/helpers/helpers';
+
+import '../components/hangmanComp/style/Hangman.css';
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 const Hangman = () => {
-    return(
-        <div></div>
+    const [playable, setPlayable] = useState(true);
+    const [correctLetters, setCorrectLetters] = useState([]);
+    const [wrongLetters, setWrongLetters] = useState([]);
+    const [showNotification, setShowNotification] = useState(false);
+
+    useEffect(() => {
+        const handleKeydown = event => {
+            const { key, keyCode } = event;
+            if (playable && keyCode >= 65 && keyCode <= 90) {
+                const letter = key.toLowerCase();
+                if (selectedWord.includes(letter)) {
+                    if (!correctLetters.includes(letter)) {
+                        setCorrectLetters(currentLetters => [...currentLetters, letter]);
+                    } else {
+                        show(setShowNotification);
+                    }
+                } else {
+                    if (!wrongLetters.includes(letter)) {
+                        setWrongLetters(currentLetters => [...currentLetters, letter]);
+                    } else {
+                        show(setShowNotification);
+                    }
+                }
+            }
+        }
+        window.addEventListener('keydown', handleKeydown);
+
+        return () => window.removeEventListener('keydown', handleKeydown);
+    }, [correctLetters, wrongLetters, playable]);
+
+    function playAgain() {
+        setPlayable(true);
+
+        // Empty Arrays
+        setCorrectLetters([]);
+        setWrongLetters([]);
+
+        const random = Math.floor(Math.random() * words.length);
+        selectedWord = words[random];
+    }
+
+    return (
+        <>
+            <Header />
+            <div className="game-container">
+                <Figure wrongLetters={wrongLetters} />
+                <WrongLetters wrongLetters={wrongLetters} />
+                <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+            </div>
+            <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+            <Notification showNotification={showNotification} />
+        </>
     );
 }
 export default Hangman
+
+
+
+
+
