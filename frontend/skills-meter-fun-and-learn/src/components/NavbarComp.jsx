5919c2320b101d56ad6f66bf1f9b6bd4646181f8
unknown <maria.popa.8735@gmail.com>
2023-10-12 10:25:16 +0300

    navbar updates

--- frontend/skills-meter-fun-and-learn/src/components/NavbarComp.jsx
+++ frontend/skills-meter-fun-and-learn/src/components/NavbarComp.jsx
@@ -13,41 +13,41 @@ export default class NavbarComp extends Component {
                 <Navbar expand="lg" className="bg-body-tertiary">
                     <Container>
                         <Nav.Link to="/" style={logoStyle}>
-                            <img height={85} className="logo" src={Temporary_Logo} alt="logo"/>
+                            <img height={100} className="logo" src={Temporary_Logo} alt="logo"/>
                         </Nav.Link>
                         <Navbar.Brand href="#home">Skills Meter</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
                                 <NavDropdown title="Games" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Tic-Tac-Toe</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Trivia</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Typing speed test</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.4">Snake</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.5">Hangman</NavDropdown.Item>
+                                    <NavDropdown.Item href="/games/tictactoe">Tic-Tac-Toe</NavDropdown.Item>
+                                    <NavDropdown.Item href="/games/trivia">Trivia</NavDropdown.Item>
+                                    <NavDropdown.Item href="/games/typingspeedtest">Typing speed test</NavDropdown.Item>
+                                    <NavDropdown.Item href="/games/snake">Snake</NavDropdown.Item>
+                                    <NavDropdown.Item href="/games/hangman">Hangman</NavDropdown.Item>
                                 </NavDropdown>
 
                                 <NavDropdown title="Skills" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/4.1">Logical Thinking</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/4.2">Spatial Awareness</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/4.3">Problem Solving</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/4.4">Math Skills</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/4.5">English Skills</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/4.6">Fine Motor Skills</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/4.7">Strategic Planning</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/4.8">Collaboration</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/4.8">Creativity (imagination, artistic expression)</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/5.1">Memory</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/5.2">Communication</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/5.3">Attention and Concentration</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/5.4">Language and Reading</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/5.5">Time management</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/5.6">Persistence and Resilience</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/logicalthinking">Logical Thinking</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/spatialawareness">Spatial Awareness</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/problemsolving">Problem Solving</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/mathskills">Math Skills</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/englishskills">English Skills</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/finemotorskills">Fine Motor Skills</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/strategicplanning">Strategic Planning</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/collaboration">Collaboration</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/creativity">Creativity (imagination, artistic expression)</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/memory">Memory</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/communication">Communication</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/attentionandconcentration">Attention and Concentration</NavDropdown.Item>
+                                    <NavDropdown.Item href="/skills/languageandreading">Language and Reading</NavDropdown.Item>
+                                    <NavDropdown.Item href="#/skills/timemanagement">Time management</NavDropdown.Item>
+                                    <NavDropdown.Item href="#/skills/persistenceandresilience">Persistence and Resilience</NavDropdown.Item>
                                 </NavDropdown>
 
-                                <Nav.Link href="#aboutUs">About us</Nav.Link>
-                                <Nav.Link href="#contact">Contact</Nav.Link>
-                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="/aboutUs">About us</Nav.Link>
+                                <Nav.Link href="#/contact">Contact</Nav.Link>
+                                <Nav.Link href="/homepage">Home</Nav.Link>
                             </Nav>
                             <Nav className="ml-auto">
                                 <Button type="submit" className="mx-2">Sign In</Button>
